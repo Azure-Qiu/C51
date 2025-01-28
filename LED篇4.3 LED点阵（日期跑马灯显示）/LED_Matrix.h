465f30a7fd666a6e7095fa3546a052ffe2d84364
Azure_Qiu <436083007@qq.com>
2025-01-28 17:12:36 +0800

    删除改名文件

--- LED篇4.3 LED点阵（日期跑马灯显示）/LED_Matrix.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LED_MATRIX_
-#define _LED_MATRIX_
-
-#include "delay.h"
-
-void LED_Init();
-void LED_Animation_Show(u8 ,u8);
-
-#endif
--- LED篇4.6 LED点阵模块功能封装/timer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __TIMER_H__
-#define __TIMER_H__
-
-#include "public.h"
-
-void TIMERx_init(u8, u16);
-
-
-#endif
